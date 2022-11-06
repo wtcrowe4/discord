6aadebdb25a1a95b7c7aa22f6be46c8bd00adb54
Thomas Crowe <wcrowe@g.clemson.edu>
2022-11-06 11:09:31 -0500

    Set up .env and .gitingnore files

--- discord/settings.py
+++ discord/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7+4!2x4&y^0gw6=lb(q5v$n&6kb)n-31&=xumj6l(90lmq^++r'
+load_dotenv()
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
