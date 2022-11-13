72d4bd1400422e01f78db07b20d510bfd8075e0f
Thomas Crowe <wcrowe@g.clemson.edu>
2022-11-13 15:41:34 -0500

    Added new image, organized static

--- discord/settings.py
+++ discord/settings.py
@@ -124,8 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    STATIC_DIR,
+    BASE_DIR / 'static',
 ]
+
+# STATIC_ROOT for user uploaded pictures.
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
