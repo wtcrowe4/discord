92ae03868d5aef66d9be574d6cfd342f5791437e
Thomas Crowe <wcrowe@g.clemson.edu>
2022-12-10 14:05:04 -0500

    static_root

--- discord/settings.py
+++ discord/settings.py
@@ -126,9 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+
 
 # STATIC_ROOT for user uploaded pictures.
 
